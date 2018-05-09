4d0be4d1120ef4bc4e2cca7c2a4985fc66cd66e2
hemanthallure <iOSEngineer@icloud.com>
2018-05-10 00:08:36 +0530

    Initial Commit

---
+++ StringTest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  StringTest
+//
+//  Created by Hemanth Alluri on 10/05/18.
+//  Copyright © 2018 Hemanth Alluri. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
